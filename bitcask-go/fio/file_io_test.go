5a04dcc92f43ba3546f20027e4771df38110f8e1
storm-crypto <1742593818@qq.com>
2023-03-14 22:15:48 +0800

    完成磁盘部分的代码 + 测试代码

---
+++ bitcask-go/fio/file_io_test.go
@@ -0,0 +1,101 @@
+package fio
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func destroyFile(name string) {
+	if err := os.RemoveAll(name); err != nil {
+		panic(err)
+	}
+}
+
+func TestNewFileIOManger(t *testing.T) {
+	path := filepath.Join("./", "a.data")
+	fio, err := NewFileIOManager(path)
+	defer destroyFile(path)
+	defer fio.Close()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+}
+
+func TestFileIO_Write(t *testing.T) {
+	path := filepath.Join("./", "a.data")
+	fio, err := NewFileIOManager(path)
+	defer destroyFile(path)
+	defer fio.Close()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	n, err := fio.Write([]byte(""))
+	assert.Equal(t, 0, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("bitcask kv"))
+	assert.Equal(t, 10, n)
+	assert.Nil(t, err)
+
+	n, err = fio.Write([]byte("storage"))
+	assert.Equal(t, 7, n)
+	assert.Nil(t, err)
+
+}
+
+func TestFileIO_Read(t *testing.T) {
+	path := filepath.Join("./", "a.data")
+	fio, err := NewFileIOManager(path)
+	defer destroyFile(path)
+	defer fio.Close()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	_, err = fio.Write([]byte("key-a"))
+	assert.Nil(t, err)
+
+	_, err = fio.Write([]byte("key-b"))
+	assert.Nil(t, err)
+
+	b1 := make([]byte, 5)
+	n, err := fio.Read(b1, 0)
+	assert.Equal(t, 5, n)
+	assert.Equal(t, []byte("key-a"), b1)
+
+	b2 := make([]byte, 5)
+	n, err = fio.Read(b2, 0)
+	assert.Equal(t, 5, n)
+	assert.Equal(t, []byte("key-a"), b2)
+
+}
+
+func TestFileIO_Sync(t *testing.T) {
+	path := filepath.Join("./", "a.data")
+	fio, err := NewFileIOManager(path)
+	defer destroyFile(path)
+	defer fio.Close()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	err = fio.Sync()
+	assert.Nil(t, err)
+
+}
+
+func TestFileIO_Close(t *testing.T) {
+	path := filepath.Join("./", "a.data")
+	fio, err := NewFileIOManager(path)
+	defer destroyFile(path)
+	defer fio.Close()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	err = fio.Close()
+	assert.Nil(t, err)
+}
